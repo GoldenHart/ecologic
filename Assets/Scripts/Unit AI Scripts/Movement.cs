03065ae11989d6c8677556d0fb2b4b686d361b9d
GoldenHart <32268947+GoldenHart@users.noreply.github.com>
2020-05-25 22:34:56 -0400

    Initial Commit

---
+++ Assets/Scripts/Unit AI Scripts/Movement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// BASE CODE FOR MOVEMENT
+/// NavMeshAgent agent = GetComponent<NavMeshAgent>();
+/// agent.destination = goal.position;
+/// </summary>
+
+public class Movement : MonoBehaviour
+{
+    public bool isHungry = false;
+    public bool isThirsty = false;
+    
+    private void Update()
+    {
+        if (isHungry)
+        {            
+            FindClosestFood();
+        }
+        else if (isThirsty)
+        {
+            FindClosestWater();
+        }
+        else
+        {
+            // Idle
+        }
+    }
+    void FindClosestFood()
+    {
+        float distanceToClosestFood = Mathf.Infinity;
+        Food closestFood = null;
+        Food[] allFood = GameObject.FindObjectsOfType<Food>();
+
+        foreach (Food currentFood in allFood)
+        {
+            float distanceToFood = (currentFood.transform.position - this.transform.position).sqrMagnitude;
+            if (distanceToFood < distanceToClosestFood)
+            {
+                distanceToClosestFood = distanceToFood;
+                closestFood = currentFood;
+            }
+        }
+        // Find Food
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = closestFood.transform.position;
+    }
+    void FindClosestWater()
+    {
+        float distanceToClosestWater = Mathf.Infinity;
+        Water closestWater = null;
+        Water[] allWater = GameObject.FindObjectsOfType<Water>();
+
+        foreach (Water currentWater in allWater)
+        {
+            float distanceToWater = (currentWater.transform.position - this.transform.position).sqrMagnitude;
+            if (distanceToWater < distanceToClosestWater)
+            {
+                distanceToClosestWater = distanceToWater;
+                closestWater = currentWater;
+            }
+        }
+        // Find Water
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = closestWater.transform.position;
+    }
+
+}
+
