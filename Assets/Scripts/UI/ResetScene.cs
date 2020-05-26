1d1851105ca313a9a546e325e90888f93b068426
GoldenHart <32268947+GoldenHart@users.noreply.github.com>
2020-05-26 15:33:18 -0400

    Added UI and Fixed Camera View

---
+++ Assets/Scripts/UI/ResetScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResetScene : MonoBehaviour
+{
+    private Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
