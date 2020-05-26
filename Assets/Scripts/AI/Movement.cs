7a164f0ef851da63f89d3a0be0ac5b3117bc691c
GoldenHart <32268947+GoldenHart@users.noreply.github.com>
2020-05-26 14:56:45 -0400

    Ecologic 0.0.1

--- Assets/Scripts/AI/Movement.cs
+++ Assets/Scripts/AI/Movement.cs
@@ -11,22 +11,32 @@ using UnityEngine.AI;
 
 public class Movement : MonoBehaviour
 {
-    public bool isHungry = false;
-    public bool isThirsty = false;
-    
+    bool isHungry = true;
+    bool isThirsty = false;
+    bool canIdle = false;
+    Vector3 home;
+
+    private void Start()
+    {
+        home = gameObject.transform.position;
+    }
     private void Update()
     {
         if (isHungry)
-        {            
+        {
+            print("I need sustenance and I am going to find it.");
             FindClosestFood();
         }
         else if (isThirsty)
         {
+            print("I need liquid sustenance now.");
             FindClosestWater();
         }
-        else
+        else if (canIdle)
         {
-            // Idle
+            print("All my sustenance has been procured. I'm going home now!");
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            agent.destination = home;
         }
     }
     void FindClosestFood()
@@ -47,6 +57,11 @@ public class Movement : MonoBehaviour
         // Find Food
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = closestFood.transform.position;
+        if (closestFood.transform.position == null)
+        {
+            print("There is no sustenance. I am dead.");
+            Destroy(gameObject);
+        }
     }
     void FindClosestWater()
     {
@@ -66,6 +81,24 @@ public class Movement : MonoBehaviour
         // Find Water
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = closestWater.transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(("Food")) && isHungry)
+        {
+            isHungry = false;
+            isThirsty = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Water") && isThirsty)
+        {
+            isThirsty = false;
+            canIdle = true;
+        }
     }
 }
 
--- Assets/Scripts/ObjectType/Food.cs
+++ Assets/Scripts/ObjectType/Food.cs
@@ -1,4 +1,12 @@
 ﻿using UnityEngine;
 
-public class Food: MonoBehaviour { }
+public class Food: MonoBehaviour {
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag(("Species")))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
 
